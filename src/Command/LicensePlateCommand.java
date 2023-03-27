cbf388a9e0b5301bc4c1f1b7c897acb07a514060
Daan Daling <73099939+DaanDaling@users.noreply.github.com>
2023-03-27 13:48:07 +0200

    [WIP] Command
    - version one of the license command
    - multiple TODO's left

--- src/Command/LicensePlateCommand.java
+++ src/Command/LicensePlateCommand.java
@@ -1,4 +1,34 @@
 package Command;
 
-public class LicensePlateCommand {
+import Adapter.APIAdapter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static API.APIBuilder.BASE_REPORT_URL;
+import static API.APIBuilder.buildUrl;
+import static API.RDWAPIClient.getResponse;
+import static Formatters.Formatter.formatLicense;
+
+public class LicensePlateCommand implements Command{
+    @Override
+    public String execute(String input) throws Exception {
+
+        // Create a HashMap to store the API parameters
+        Map<String, String> params = new HashMap<>();
+        params.put("kenteken", formatLicense(input));
+
+        String url = buildUrl(BASE_REPORT_URL ,params);
+
+        // TODO: Check if it already exist (momento) before performing call
+
+        String response = getResponse((url));
+
+        // Adapt the API response to a HashMap of key-value pairs
+        HashMap<String, String> vehicleData = new APIAdapter().adaptResponse(response);
+
+        //TODO: Put data into a report
+
+        //TODO: Return and open report
+        return response;
+    }
 }
