d5c369755928b428366c8500225a5d66409a1fe1
Daan Daling <73099939+DaanDaling@users.noreply.github.com>
2023-03-27 11:41:18 +0200

    [WIP] APIBuilder
    - First version of the API Builder

--- src/API/APIBuilder.java
+++ src/API/APIBuilder.java
@@ -3,4 +3,24 @@ package API;
 import java.util.Map;
 public class APIBuilder {
 
+    // Base URL for the RDW Open Data API
+    public static final String BASE_REPORT_URL = "https://opendata.rdw.nl/resource/m9d7-ebf2";
+
+    // Parameter keys for constructing the query string
+    public static final String TRADE_NAME_PARAM = "&handelsbenaming=";
+    public static final String FIRST_ADMISSION_PARAM = "&datum_eerste_toelating=";
+    public static final String LICENSE_PARAM = "&kenteken=";
+
+    // Builds a URL string for querying the RDW Open Data API with the given parameters.
+    public static String buildUrl(String URL, Map<String, String> params) {
+        StringBuilder builder = new StringBuilder(URL);
+        builder.append("?");
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            builder.append(entry.getKey())
+                    .append("=")
+                    .append(entry.getValue())
+                    .append("&");
+        }
+        return builder.toString();
+    }
 }
