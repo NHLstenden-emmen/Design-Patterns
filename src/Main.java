7652bfe545b14fb9ed2633e411b613bc51440a9b
dreddyaell <72259796+dreddyaell@users.noreply.github.com>
2023-03-27 14:28:14 +0200

    raport stukje yaell

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import API.APIBuilder;
 import Adapter.APIAdapter;
 import Memento.RDWResponseCache;
+import RDWReportBuilder.ReportBuilder;
 
 import java.io.FileWriter;
 import java.util.HashMap;
@@ -12,36 +13,11 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Map<String, String> params = new HashMap<>();
-        params.put("kenteken", "TRHP81");
+            String kenteken = "TRHP81";
+            RDWResponseCache cache = new RDWResponseCache();
 
-        String kenteken = params.get("kenteken");
-
-        RDWResponseCache cache = new RDWResponseCache();
-        HashMap<String, String> cachedResponse = cache.getResponse(kenteken);
-
-        String filename = kenteken; // set desired filename here
-
-        FileWriter writer = new FileWriter(filename);
-
-        if (cachedResponse != null) {
-            // Use cached response
-            for (Map.Entry<String, String> entry : cachedResponse.entrySet()) {
-                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
-            }
-        } else {
-            // Make API call and store response in cache
-            String url = APIBuilder.buildUrl(APIBuilder.BASE_REPORT_URL, params);
-            String response = getResponse(url);
-            HashMap<String, String> vehicleData = new APIAdapter().adaptResponse(response);
-            cache.addResponse(kenteken, vehicleData);
-            for (Map.Entry<String, String> entry : vehicleData.entrySet()) {
-                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
-            }
+            ReportBuilder.buildReport(kenteken, cache);
         }
-
-        writer.close();
-        System.out.println("Successfully saved data to " + filename);
     }
-}
+
 
--- src/RDWReportBuilder/ReportBuilder.java
+++ src/RDWReportBuilder/ReportBuilder.java
@@ -4,29 +4,43 @@ import API.APIBuilder;
 import Adapter.APIAdapter;
 import Memento.RDWResponseCache;
 
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 import static API.RDWAPIClient.getResponse;
 
 public class ReportBuilder {
-        public static HashMap<String, String> getVehicleData(String kenteken, RDWResponseCache cache) throws Exception {
-            HashMap<String, String> vehicleData;
+    public static void buildReport(String kenteken, RDWResponseCache cache) throws Exception {
+        HashMap<String, String> vehicleData = getVehicleData(kenteken, cache);
+        String filename = "report_" + kenteken + ".txt";
 
-            HashMap<String, String> cachedResponse = cache.getResponse(kenteken);
-            if (cachedResponse != null) {
-                // Use cached response
-                vehicleData = cachedResponse;
-            } else {
-                // Make API call and store response in cache
-                Map<String, String> params = new HashMap<>();
-                params.put("kenteken", kenteken);
-                String url = APIBuilder.buildUrl(APIBuilder.BASE_REPORT_URL, params);
-                String response = getResponse(url);
-                vehicleData = new APIAdapter().adaptResponse(response);
-                cache.addResponse(kenteken, vehicleData);
-            }
-            return vehicleData;
+        FileWriter writer = new FileWriter(filename);
+
+        for (Map.Entry<String, String> entry : vehicleData.entrySet()) {
+            writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
         }
+
+        writer.close();
+        System.out.println("Successfully saved data to " + filename);
     }
 
+    public static HashMap<String, String> getVehicleData(String kenteken, RDWResponseCache cache) throws Exception {
+        HashMap<String, String> vehicleData;
+
+        HashMap<String, String> cachedResponse = cache.getResponse(kenteken);
+        if (cachedResponse != null) {
+            // Use cached response
+            vehicleData = cachedResponse;
+        } else {
+            // Make API call and store response in cache
+            Map<String, String> params = new HashMap<>();
+            params.put("kenteken", kenteken);
+            String url = APIBuilder.buildUrl(APIBuilder.BASE_REPORT_URL, params);
+            String response = getResponse(url);
+            vehicleData = new APIAdapter().adaptResponse(response);
+            cache.addResponse(kenteken, vehicleData);
+        }
+        return vehicleData;
+    }
+}
