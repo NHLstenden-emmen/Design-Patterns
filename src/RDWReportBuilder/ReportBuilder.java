a8070cf9eab7022163ead9e81c1dd7430ab5f318
Daan Daling <73099939+DaanDaling@users.noreply.github.com>
2023-03-30 16:25:49 +0200

    spacing

--- src/RDWReportBuilder/ReportBuilder.java
+++ src/RDWReportBuilder/ReportBuilder.java
@@ -31,26 +31,27 @@ public class ReportBuilder {
         }
     }
 
-        public static void buildReportMerk(String tradeName, RDWResponseCache cache) {
-            Command.TradeNameCommand command = new TradeNameCommand();
-            try {
-                HashMap<String, String> vehicleData = command.execute(tradeName);
+    public static void buildReportMerk(String tradeName, RDWResponseCache cache) {
+        Command.TradeNameCommand command = new TradeNameCommand();
+        try {
+            HashMap<String, String> vehicleData = command.execute(tradeName);
 
-                // Build report using the vehicleData HashMap
-                // For example, you could write the data to a file:
-                String filename = "report_" + tradeName + ".txt";
-                FileWriter writer = new FileWriter(filename);
-                for (Map.Entry<String, String> entry : vehicleData.entrySet()) {
-                    writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
-                }
-                writer.close();
-                System.out.println("Successfully saved data to " + filename);
-
-            } catch (Exception e) {
-                // Handle the exception
-                e.printStackTrace();
+            // Build report using the vehicleData HashMap
+            // For example, you could write the data to a file:
+            String filename = "report_" + tradeName + ".txt";
+            FileWriter writer = new FileWriter(filename);
+            for (Map.Entry<String, String> entry : vehicleData.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
             }
+            writer.close();
+            System.out.println("Successfully saved data to " + filename);
+
+        } catch (Exception e) {
+            // Handle the exception
+            e.printStackTrace();
         }
+    }
+
     public static void buildReportFirstAdmission(String firstAdmission, RDWResponseCache cache) {
        Command.FirstAdmissionCommand command = new FirstAdmissionCommand();
         try {
